c6ece6f449df516e02ea8de7647f1967c3d4016b
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-03-18 21:24:50 +0100

    Add python script

---
+++ scripts/01-variables.py
@@ -0,0 +1,55 @@
+"""
+var = 123
+print(var)
+# print(Var)
+
+print("Hello World") # This line of code prints Hello World
+
+"""
+# # Integer
+# a = 1
+# print(a)
+# print(type(a))
+
+# # Float
+# b = 2.5
+# print(b)
+# print(type(b))
+
+# # Complex
+# c = 2 + 3j
+# print(c)
+# print(type(c))
+
+
+## Boolean Datatype
+# a = True
+# print(a)
+# print(type(a))
+
+# b = False
+# print(b)
+# print(type(b))
+
+## String datatype
+# a = '123'
+# print(type(a))
+# print(a)
+
+# b = 'Hello World'
+# print(b)
+
+# c = 'Today\'s weather is nice'
+# print(c)
+
+# d = """
+# - Python is case-sensitive which means python treats A and a as different
+# - To comment a line of code in python, we use: # -> Single line comment
+# - Similarly, you can also comment out a block of code using:
+# """
+# print(d)
+
+# a = "This is 
+# a multiline"
+# print(a)
+
